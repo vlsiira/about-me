9e976aae209f722cd9a1f8d9c88c0bbca1676b09
Valerie Siira <vlsiira@vt.edu>
2018-01-30 13:48:59 -0800

    added files, moved JS

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const birthCity = prompt('What city were you born in?')
+alert('You were born in ' + birthCity)
+console.log('birth city is: ' + birthCity)
+
+const favFood = prompt('What is your favorite food?')
+alert('You were born in ' + birthCity + ', where you like to eat ' + favFood)
+console.log('favorite food is: ' + favFood)
+
+const cutestAnimal = prompt('What is the cutest animal?')
+alert('You were born in ' + birthCity + ', where you like to eat ' + favFood + ' with ' + cutestAnimal)
+console.log('cutest animal is: ' + cutestAnimal)
+
+const amountChickens = prompt('How many chickens do you have?')
+alert('You were born in ' + birthCity + ' where you like to eat ' + favFood + ' with ' + cutestAnimal + ' and ' + amountChickens + ' chickens')
+console.log('number of chickens: ' + amountChickens)
